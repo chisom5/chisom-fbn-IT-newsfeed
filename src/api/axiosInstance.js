51309d0d203ef51c400a6a6098fdbb49c63ddbd6
chisom okoye <okoyechisom54@gmail.com>
2026-09-12 16:53:34 +0100

    change proxy

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = import.meta.env.PROD
-  ? "https://corsproxy.io/?https://newsapi.org/v2"
+  ? "https://api.allorigins.win/raw?url=https://newsapi.org/v2"
   : "/api";
 
 const axiosInstance = axios.create({
