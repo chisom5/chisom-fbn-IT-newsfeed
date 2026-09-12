670f72ee064441ee0633cc963595d34b9eadd1a8
chisom okoye <okoyechisom54@gmail.com>
2026-09-12 16:34:00 +0100

    added config for vercel deployment

--- src/api/news.js
+++ src/api/news.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  const { country, pageSize, page } = req.query;
+  const { country, pageSize, page, q } = req.query;
   const apiKey = process.env.NEWS_API_KEY; 
 
   try {
